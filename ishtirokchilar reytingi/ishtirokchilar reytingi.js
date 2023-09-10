0cfc3c4531912de64d62a83d0011757609ec0797
muhammaddin <muhammaddin.xoliqov1998@gmail.com>
2023-09-10 12:39:25 +0500

    new page

--- ishtirokchilar reytingi/ishtirokchilar reytingi.js
+++ ishtirokchilar reytingi/ishtirokchilar reytingi.js
@@ -1,6 +1,7 @@
 const table = document.querySelector('.table')
 const tbody = document.querySelector('.tbody')
 const api_url = './data.json'
+let n = 1
 
 
 // malumotlarni apida olib keladi
@@ -8,9 +9,9 @@ function dataApi() {
    fetch(api_url)
       .then((res) => res.json())
       .then((data) => {
-         getArr(data.data);
+         generateAll(data.data);
       })
-      .catch(() => alert("Xatolik yuz berdi!"));
+      .catch(() => alert("Ma'lumot kelishda xatolik yuz berdi!"));
 }
 dataApi()
 
@@ -24,12 +25,12 @@ function generateAll(res) {
 
 
       tableBody.innerHTML = `
-      <td class="border-2 border-black">
+      <td class="border-2 border-x-0 border-black">
          <div class="flex items-center justify-center">
             ${n++}
          </div>
       </td>
-      <td class="border-2 border-black px-8 py-1 md:py-3">
+      <td class="border-2 border-x-0 border-black px-8 py-1 md:py-3">
          <div class="flex items-center gap-5">
             <div
                class="flex min-w-[70px] md:min-w-[123px] min-h-[60px] md:min-h-[102px] justify-center items-center bg-white border-solid border-2 border-gray-200 rounded-lg">
@@ -39,13 +40,8 @@ function generateAll(res) {
             <p class="md:w-56">${item.name}</p>
          </div>
       </td>
-      <td class="border-2 border-black px-5">
-         <div class="flex mt-1 gap-1 text-xs md:text-sm justify-center ">
-            <img src="../logo and icons/ishtirokchilar reytingi/location.svg" alt="">
-            <p>${item.hudud}</p>
-         </div>
-      </td>
-      <td class="border-2 border-black">
+      
+      <td class="border-2 border-x-0  border-black">
          <div class="flex justify-center">
             <p
                class="ball w-[51px] h-[28px] bg-gray-100 flex justify-center items-center rounded-lg font-medium">
@@ -53,7 +49,7 @@ function generateAll(res) {
             </p>
          </div>
       </td>
-      <td class="border-2 border-black">
+      <td class="border-2 border-s-0 border-black">
       <div class="w-full h-full flex justify-center items-center px-3">
          <button
             class="uppercase py-2 px-6 rounded-lg font-medium text-blue-500 border-2 hover:bg-[#2861F5] hover:text-white border-blue-500">batafsil</button>
